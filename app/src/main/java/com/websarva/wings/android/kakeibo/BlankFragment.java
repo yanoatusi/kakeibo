5b48a9a0a52fa08b9478ca43b13c920f1289f7df
yanoatusi <rG734jQyup2A@outlook.jp>
2019-11-11 12:03:19 +0900

    円グラフ拡大追加

--- app/src/main/java/com/websarva/wings/android/kakeibo/BlankFragment.java
+++ app/src/main/java/com/websarva/wings/android/kakeibo/BlankFragment.java
@@ -1,18 +1,11 @@
 package com.websarva.wings.android.kakeibo;
 
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
-import android.icu.util.ULocale;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.design.bottomappbar.BottomAppBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AlertDialog;
@@ -24,25 +17,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Calendar;
-
-import com.websarva.wings.android.kakeibo.R;
 
 /**
  * A simple {@link Fragment} subclass.
